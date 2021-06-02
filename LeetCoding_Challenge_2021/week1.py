8ded6cd9b76ba9ace797527d9645ac46950ee60b
Lia Lee <lia.lee@nitmus.com>
2021-06-02 18:55:02 +0900

    LeetCoding  Challenge 2021 week1

---
+++ LeetCoding_Challenge_2021/week1.py
@@ -0,0 +1,11 @@
+# Max Area of Island
+
+
+class Solution:
+	
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        for y in range(len(grid)):
+			for x in range(len(grid[y])):
+				if grid[y][x] == 1:
+					pass
+
