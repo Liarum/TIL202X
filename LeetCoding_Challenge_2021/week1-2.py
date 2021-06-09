6f303eb5315c2400f48b656a10a3c9ec737fafa6
Lia Lee <lia.lee@nitmus.com>
2021-06-09 13:00:11 +0900

    challenge w1-2, w2-1

---
+++ LeetCoding_Challenge_2021/week1-2.py
@@ -0,0 +1,17 @@
+# Interleaving String
+
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        pass
+        
+
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.isInterleave())
