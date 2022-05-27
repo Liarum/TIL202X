87a670a05ff16d0bd3a5e0df93b337eadd00c9f3
Liarum <lar404@naver.com>
2022-05-27 22:01:10 +0900

    doing data visualizaiton assingment

---
+++ R/final_ex.R
@@ -0,0 +1,11 @@
+#############################
+## 데이터 시각화 기말 과제 ##
+#############################
+
+
+# 3.
+install.packages("vcd", repos="http://R-Forge.R-project.org")
+install.packages("lmtest")
+installed.packages()
+
+library(vcd) # todo: install gfrotran and install lmtest
