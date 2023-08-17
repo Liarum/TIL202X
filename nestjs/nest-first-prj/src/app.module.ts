0f86eeab36c4d1ee1db4310223247fbc516353cb
Liarum <lar404@naver.com>
2023-08-18 01:25:51 +0900

    learning nestjs | create project, controller

---
+++ nestjs/nest-first-prj/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+
+@Module({
+  imports: [], //[UsersModule],
+  controllers: [AppController, UsersController], //[AppController, UsersController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
---
+++ nestjs/nest-first-prj/src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Res,
+  Controller, Get, Post, Body, Patch, Param, Delete,
+  HttpCode, BadRequestException
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll();
+    return res.status(200).send(users); // this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
+    return this.usersService.findOne(+id);
+  }
+
+  @HttpCode(202)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
